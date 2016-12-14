41f78751bb8f0da107f202dc607f7c027b690924
RayoRodriguez <rayoara@hotmail.com>
2016-12-14 00:39:57 -0700

    Initial commit

---
+++ app/src/main/java/edu/tecii/android/practicafragments/FragmentCallbacks.java
@@ -0,0 +1,8 @@
+package edu.tecii.android.practicafragments;
+
+/**
+ * Created by Maritza on 31/10/2016.
+ */
+public interface FragmentCallbacks {
+    void onMsgFromMainToFragment(String strValue);
+}
---
+++ app/src/main/java/edu/tecii/android/practicafragments/MainCallbacks.java
@@ -0,0 +1,8 @@
+package edu.tecii.android.practicafragments;
+
+/**
+ * Created by Maritza on 31/10/2016.
+ */
+public interface MainCallbacks {
+    void onMsgFromFragToMain(String sender, String strValue);
+}
